773984e388778f744ddb735d716551fabc352846
FasterThanIE <tomicawarr@gmail.com>
2020-12-02 00:39:03 +0100

    * Adding initial entities

---
+++ App/Entities/MenuEntity.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Entities;
+
+abstract class MenuEntity
+{
+    /**
+     * @var string
+     */
+    protected string $name;
+
+    /**
+     * @var float
+     */
+    protected float $price;
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param float $price
+     */
+    public function setPrice(float $price): void
+    {
+        $this->price = $price;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+}
--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
 
 require_once "vendor/autoload.php";
+
+$menu = new \App\Builders\MenuBuilder();
